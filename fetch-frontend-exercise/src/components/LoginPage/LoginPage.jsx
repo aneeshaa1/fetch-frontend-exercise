e3db5826f2822e29da4a74beae003932e8c10871
aneeshaa1 <acharyannie@gmail.com>
2025-06-03 03:49:16 -0400

    login functionality

--- fetch-frontend-exercise/src/components/LoginPage/LoginPage.jsx
+++ fetch-frontend-exercise/src/components/LoginPage/LoginPage.jsx
@@ -1,7 +1,53 @@
+import React, {useState} from "react";
+import { useNavigate } from "react-router-dom";
+
 function LoginPage() {
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const navigate = useNavigate();
+
+    const handleLogin = async () => {
+        const response = await fetch('https://frontend-take-home-service.fetch.com/auth/login',
+            {
+                method: 'POST',
+                credentials: 'include',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({name, email})
+            }
+        );
+
+        if (response.ok) {
+            navigate('/browse');
+        } else {
+            alert('login failed :( try again')
+        }
+    };
+
     return (
         <div>
             <h1>Login Page</h1>
+
+            <input
+                type="text"
+                placeholder="name"
+                value={name}
+                onChange={e => setName(e.target.value)}
+            />
+
+            <br></br>
+
+            <input
+                type="email"
+                placeholder="email"
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+            />
+
+            <br></br>
+
+            <button onClick={handleLogin}>Login</button>
+
         </div>
     )
 }
